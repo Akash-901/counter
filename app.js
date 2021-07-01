bab4105e1a08813744a3532639f40905af80e07b
Akash <kumarakash.ka650@gmail.com>
2021-07-01 18:54:27 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,21 @@
+let count = 0;
+
+var value = document.querySelector("#value");
+var btns = document.querySelectorAll(".btn");
+
+btns.forEach(function (btn) {
+    btn.addEventListener("click", function (e) {
+        const styles = e.currentTarget.classList;
+        //console.log(styles);
+        if (styles.contains("decrease")) {
+            count--;
+        }
+        else if (styles.contains("increase")) {
+            count++;
+        }
+        else {
+            count = 0;
+        }
+        value.innerHTML = count;
+    })
+});
